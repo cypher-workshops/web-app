102b5ffa41d1c4f9f2f7c7b2bbf4eca71e28db8c
22-Brooke <brooke.vella@pcds.org>
2025-02-04 16:21:44 -0700

    Star/Bones

--- frontend/src/components/Components.jsx
+++ frontend/src/components/Components.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
-import './Components.css'; // Import CSS
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'; // Import MUI components
 
-function Components() {
-  return <div className="components-page">Get creative!</div>;
+export default function MediaCard() {
+  return (
+    <Card sx={{maxWidth: 345}}>
+      <CardMedia
+        sx={{ height: 200, width: 200, justifySelf: "center" }} //styling for the image 
+        image="/src/assets/bonestime.jpg" /*this is the path to your image*/
+        title="cypher alien"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Bones!!
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Temperence Brennan and Seely Booth
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Season 4</Button> {/*implement functionality!*/}
+        <Button size="small">Episode 17</Button> {/*implement functionality!*/}
+      </CardActions>
+
+      <CardMedia
+        sx={{ height: 300, width: 300, justifySelf: "center" }} //styling for the image 
+        image="/src/assets/bonesbraintumor.jpg" /*this is the path to your image*/
+        title="cypher alien"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Bones!!
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Temperence Brennan and Seely Booth
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Season 4</Button> {/*implement functionality!*/}
+        <Button size="small">Episode 21</Button> {/*implement functionality!*/}
+      </CardActions>
+
+      <CardMedia
+        sx={{ height: 300, width: 300, justifySelf: "center" }} //styling for the image 
+        image="/src/assets/bonessexyscientist.jpg" /*this is the path to your image*/
+        title="cypher alien"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Bones!!
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Temperence Brennan and Seely Booth
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Season 4</Button> {/*implement functionality!*/}
+        <Button size="small">Episode 21</Button> {/*implement functionality!*/}
+      </CardActions>
+    </Card>
+    
+  );
 }
-
-export default Components;
--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import './HomePage.css';  // Import the HomePage CSS
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'; // Import MUI components
 
-function HomePage() {
-  return <div className="home-page">Welcome!</div>;
+export default function MediaCard() {
+  return (
+    <Card sx={{maxWidth: 345}}>
+      <CardMedia
+        sx={{ height: 200, width: 200, justifySelf: "center" }} //styling for the image 
+        image="/src/assets/myimage.png" /*this is the path to your image*/
+        title="cypher alien"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Neutron Star Collision
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          This is actually so cool!
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">This is</Button> {/*implement functionality!*/}
+        <Button size="small">Awesome</Button> {/*implement functionality!*/}
+      </CardActions>
+    </Card>
+  );
 }
-
-export default HomePage;
