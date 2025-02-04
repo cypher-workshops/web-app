b351b09b25762dea64a07a5b447f0fd37eebe3fb
Adeline7109 <adeline.ward@pcds.org>
2025-02-04 16:23:42 -0700

    table example

--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import './HomePage.css';  // Import the HomePage CSS
+import React from 'react'; 
+import { useState } from 'react'; 
+import './Homepage.css'; 
+import { Box, Typography, TextField, Button } from '@mui/material'; 
 
-function HomePage() {
-  return <div className="home-page">Welcome!</div>;
+function HomePage() { 
+  const [name, setName] = useState(''); 
+  const [greeting, setGreeting] = useState(''); // Declare a state variable `greeting` (an empty string) with `setGreeting` to update it
+  const handleGreet = () => setGreeting(`Hi, ${name}!`); // A function that updates the `greeting` state and creates the message using `name` state.
+
+  return ( 
+    <Box status
+      className="home-page">
+      <Typography variant="h4" color="primary" gutterBottom>
+        Enter Your Name
+      </Typography>
+      <TextField
+        variant="outlined"
+        placeholder="Your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)} 
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleGreet} 
+      >
+        Greet
+      </Button>
+      {greeting && ( 
+        <Typography variant="h4" color="secondary">
+          {greeting} 
+        </Typography>
+      )}
+    </Box>
+  );
 }
 
-export default HomePage;
+
+export default HomePage;  
+
