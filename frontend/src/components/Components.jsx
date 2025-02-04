376c87c19d054deeef46c202e6cab9c86c290830
MiaCov23 <mia.coviello@pcds.org>
2025-02-03 21:14:55 -0700

    Message

--- frontend/src/components/Components.jsx
+++ frontend/src/components/Components.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Components.css'; // Import CSS
 
 function Components() {
-  return <div className="components-page">Get creative!</div>;
+  return <div className="components-page">Chocolate is fun!</div>;
 }
 
 export default Components;
--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HomePage.css';  // Import the HomePage CSS
 
 function HomePage() {
-  return <div className="home-page">Welcome!</div>;
+  return <div className="home-page">Hello!</div>;
 }
 
 export default HomePage;
