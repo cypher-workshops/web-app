eb86086f1600f15f9f7250f10e969873c32ddbc0
AlainaKieran <alaina.kieran@pcds.org>
2025-02-04 16:21:02 -0700

    Commit message

--- frontend/src/components/Components.jsx
+++ frontend/src/components/Components.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
-import './Components.css'; // Import CSS
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'; // Import MUI components
 
-function Components() {
-  return <div className="components-page">Get creative!</div>;
+export default function MediaCard() {
+  return (
+    <Card sx={{maxWidth: 500}}>
+      <CardMedia
+        sx={{ height: 400, width: 400, justifySelf: "center" }} //styling for the image 
+        image="/src/assets/myimage.jpg" /*this is the path to your image*/
+        title="cypher alien"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Grey's Anatomy
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          (the best show ever)
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Season 1</Button> {/*implement functionality!*/}
+        <Button size="small">Episode 1</Button> {/*implement functionality!*/}
+      </CardActions>
+      <CardMedia
+        sx={{ height: 400, width: 400, justifySelf: "center" }} //styling for the image 
+        image="/src/assets/myimage.jpg" /*this is the path to your image*/
+        title="cypher alien"
+      />
+      <CardContent> 
+        <Typography gutterLeft variant="h5" component="div">
+          Grey's Anatomy
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          (the best show ever)
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Season 1</Button> {/*implement functionality!*/}
+        <Button size="small">Episode 1</Button> {/*implement functionality!*/}
+      </CardActions>
+    </Card>
+  );
 }
-
-export default Components;
--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -1,39 +1,37 @@
-import React from 'react'; 
-import { useState } from 'react'; 
-import './Homepage.css'; 
-import { Box, Typography, TextField, Button } from '@mui/material'; 
+import * as React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
-function HomePage() { 
-  const [name, setName] = useState(''); 
-  const [greeting, setGreeting] = useState(''); // Declare a state variable `greeting` (an empty string) with `setGreeting` to update it
-  const handleGreet = () => setGreeting(`Hi, ${name}!`); // A function that updates the `greeting` state and creates the message using `name` state.
-
-  return ( 
-    <Box
-      className="home-page">
-      <Typography variant="h4" color="primary" gutterBottom>
-        Enter Your Name
-      </Typography>
-      <TextField
-        variant="outlined"
-        placeholder="Your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)} 
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleGreet} // Calls the handle greet function when the button is clicked 
-      >
-        Greet
-      </Button>
-      {greeting && ( // Renders the Typography text if greeting is not empty 
-        <Typography variant="h4" color="secondary">
-          {greeting}
-        </Typography>
-      )}
-    </Box>
-  );
+function createData(name, age, daysuntilpermit) {
+  return { name, age, daysuntilpermit };
 }
 
-export default HomePage;  
+const rows = [
+  createData('Alaina', 15, '9'),
+  createData('Brooke', 15, '-127'),
+  createData('Cypher Alien', 0, 'Me!'),
+];
+
+export default function DenseTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Age (years)</TableCell>
+            <TableCell align="right">days until permit</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">{row.name}</TableCell>
+              <TableCell align="right">{row.age}</TableCell>
+              <TableCell align="right">{row.daysuntilpermit}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
